8e4ff5907f6278dcc33ce71691d57ee71b8fe4fc
Begum Dogru 00819096 <begum.dogru1@huawei.com>
2023-09-23 18:20:19 +0300

    building the project

---
+++ src/main/java/com/example/flightbookingsystem/FlightBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flightbookingsystem/FlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
